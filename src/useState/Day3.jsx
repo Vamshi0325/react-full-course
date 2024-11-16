e25bd66eea87ed067c58ed3751de40ef584ac7c9
Vamshi <vamshi.vade1@gmail.com>
2024-11-17 00:09:14 +0530

    first commit

--- src/useState/Day3.jsx
+++ src/useState/Day3.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Day3 = () => {
+  const [showData, setShowData] = useState(false);
+  const handleShowData = () => {
+    setShowData((prev) => !prev);
+  };
   return (
     <div>
-      <h3>Day3</h3>
+      <button
+        className="btn btn-primary mt-3 fs-3 mb-2 w-25"
+        onClick={handleShowData}
+      >
+        {showData ? "Hide" : "Show"}
+      </button>
+      {showData && (
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum
+          dolor, sit amet consectetur adipisicing elit. A tempore illum natus
+          beatae nulla dignissimos dolores voluptatibus nesciunt quae inventore
+          ut repellat ea suscipit repudiandae culpa eius, aliquid maiores Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Inventore, sunt.
+          Soluta, id.
+        </h3>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Day3
+export default Day3;
